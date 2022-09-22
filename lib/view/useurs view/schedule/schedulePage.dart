ff2bee7ee7a1f2f874e5ec1365a66a804991442a
WL-Yahata <106352826+WL-Yahata@users.noreply.github.com>
2022-09-22 17:43:19 +0900

    a

--- lib/view/useurs view/schedule/schedulePage.dart
+++ lib/view/useurs view/schedule/schedulePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class SchedulePage extends StatefulWidget {
   const SchedulePage({Key? key}) : super(key: key);
@@ -10,6 +12,58 @@ class SchedulePage extends StatefulWidget {
 class _SchedulePageState extends State<SchedulePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'schedule',
+          style: TextStyle(color: Colors.black54),
+        ),
+        backgroundColor: Theme.of(context).canvasColor,
+        elevation: 1,
+      ),
+      body: Container(
+        child: SfCalendar(
+          view: CalendarView.month,
+          monthViewSettings: const MonthViewSettings(
+            appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
+            showAgenda: true,
+            showTrailingAndLeadingDates: false,
+          ),
+          dataSource: _getCalendarDataSource(),
+        ),
+      ),
+    );
+  }
+
+  _AppointmentDataSource _getCalendarDataSource() {
+    List<Appointment> appointments = <Appointment>[];
+
+    appointments.add(Appointment(
+        startTime: DateTime(2022, 7, 4, 16),
+        endTime: DateTime(2022, 7, 4, 18),
+        subject: 'Monday 16:00~',
+        color: Colors.blue.shade200,
+        recurrenceRule: 'FREQ=DAILY;INTERVAL=7;COUNT=70'));
+    appointments.add(Appointment(
+        startTime: DateTime(2022, 7, 4, 18),
+        endTime: DateTime(2022, 7, 4, 20),
+        subject: 'Monday 18:00~',
+        color: Colors.blue,
+        recurrenceRule: 'FREQ=DAILY;INTERVAL=7;COUNT=70'));
+    appointments.add(Appointment(
+        startTime: DateTime(2022, 7, 4, 20),
+        endTime: DateTime(2022, 7, 4, 22),
+        subject: 'Monday 20:00~',
+        color: Colors.blue.shade300,
+        recurrenceRule: 'FREQ=DAILY;INTERVAL=7;COUNT=70'));
+
+    return _AppointmentDataSource(appointments);
+  }
+}
+
+class _AppointmentDataSource extends CalendarDataSource {
+  _AppointmentDataSource(List<Appointment> source) {
+    appointments = source;
   }
 }
