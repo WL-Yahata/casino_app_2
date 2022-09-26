118e0278bc0a5550b7d371e2188678e9c5b8baed
WL-Yahata <106352826+WL-Yahata@users.noreply.github.com>
2022-09-26 09:15:30 +0900

    a

---
+++ lib/view/owners view/owners Schedule/extraSchedulePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExtraSchedulePage extends StatefulWidget {
+  const ExtraSchedulePage({Key? key}) : super(key: key);
+
+  @override
+  State<ExtraSchedulePage> createState() => _ExtraSchedulePageState();
+}
+
+class _ExtraSchedulePageState extends State<ExtraSchedulePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
