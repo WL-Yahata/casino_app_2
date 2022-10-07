e1016f59e15bf30929a1c8ae35dcf63669e06c8e
WL-Yahata <106352826+WL-Yahata@users.noreply.github.com>
2022-10-07 18:57:15 +0900

    a

---
+++ lib/view/owners view/owners Schedule/addTournament.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddTournamentPage extends StatefulWidget {
+  const AddTournamentPage({Key? key}) : super(key: key);
+
+  @override
+  State<AddTournamentPage> createState() => _AddTournamentPageState();
+}
+
+class _AddTournamentPageState extends State<AddTournamentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
